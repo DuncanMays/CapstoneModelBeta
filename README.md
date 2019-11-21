290a4f97f8a413017a10ba98bd34654d61ef6da5
duncan <duncanmays@outlook.com>
2019-11-21 00:07:44 -0500

    played around with the stpchastic markovian idea

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # CapstoneModelBeta
-Capstone Project
---
+++ main.py
@@ -0,0 +1,11 @@
+baseline = 9779
+
+
+# main program loop
+while(True):
+	# production from nuclear and hydro plants is added to the grid
+	totalProduction = baseline
+
+	# both stochastic consumers and producers determine the amount they consume and produce respectively
+
+	# production from gas-fired plants is added to the grid
