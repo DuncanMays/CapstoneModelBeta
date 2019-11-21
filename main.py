33ecf1919395c03a6fd9c8fdf766da31d73e67e1
Duncan Mays <16djm1@queensu.ca>
2019-11-21 10:04:19 -0500

    supply and demand dynamics complete, still gotta add pricing mechanisms

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from matplotlib import pyplot as plt
 from numpy import arange
 from numpy.random import normal
-from markovSources import MarkovSource
+from markovSources import House, Factory
 import math
 
 # this array holds all the times throughout the day that our model will iterate
@@ -17,20 +17,44 @@ baseline = 9779
 # we should definetely look at IESO data and find a function that fits it better 
 hydroSchedule = lambda x : 1900*math.sin(x*math.pi/12) + 1900
 
+numHouses = 450
+numFactories = 25
+
+stochasticConsumers = []
+for i in range(0,numHouses):
+	stochasticConsumers.append(House())
+for i in range(0,numFactories):
+	stochasticConsumers.append(Factory())
+
+demand = []
+production = []
+
 # main program loop
 # each iteration of this loop represents one day in the model
 # while(True):
-# for dummy in range(0,100):
-# 	# each iteration in this loop represents one time interval
-# 	for t in T:
-# 		totalProduction = 0
-# 		totalDemand = 0
+for dummy in range(0,1):
+	# each iteration in this loop represents one time interval
+	for t in T:
+		totalProduction = 0
+		totalDemand = 0
 
-# 		# production from nuclear and hydro plants is added to the grid
-# 		totalProduction += baseline
-# 		totalProduction += hydroSchedule(t)
+		# production from nuclear and hydro plants is added to the grid
+		totalProduction += baseline
+		totalProduction += hydroSchedule(t)
 
-# 		# both stochastic consumers and producers determine the amount they consume and 
-# 		# produce respectively
+		# both stochastic consumers and producers determine the amount they consume and 
+		# produce respectively
+		for consumer in stochasticConsumers:
+			totalDemand += consumer.update(t)
 
-# 		# production from gas-fired plants is added to the grid
+		# production from gas-fired plants is added to the grid
+		if(totalDemand > totalProduction):
+			totalProduction = totalDemand
+
+		demand.append(totalDemand)
+		production.append(totalProduction)
+
+
+plt.plot(T, demand)
+plt.plot(T, production)
+plt.show()
--- markovSources.py
+++ markovSources.py
@@ -3,14 +3,40 @@ import math
 
 class MarkovSource:
 
-	# 3 pieces of information are needed to instantiate a Markov Source, the 'schedule'
-	
+	# 3 pieces of information are needed to instantiate a Markov Source, the meanFunction,
+	# which is the 'schedule' that its expected value will follow, with markov disturbances,
+	# the variance, which is the variance of the gaussian distribution that gives the markov
+	# disturbance, and the initial condition, which by default is set to zero.
 	def __init__(self, meanFunction, variance, initialCondition = 0):
+		# allows these parameters to be accessed by other methods
 		self.variance = variance
 		self.meanFunction = meanFunction
 		self.state = initialCondition
 
+	# This method moves the source from one state to the next.
+	# It is necessary that it takes time as a parameter, it must know the time to know what
+	# its expected value should be, and it cannot determine the time on its own since the 
+	# main program loop needs to be able to control intervals and such.
 	def update(self, time):
+		# calculates the difference between the current state and the desired expected value
 		diff = self.state - self.meanFunction(time)
+
+		# changes the state by a gaussian random variabe with variance as defined in __init__
+		# and mean that's halfway between the current state and desired expected value.
 		self.state -= normal(diff/2, self.variance)
+
+		# forces the state to be positive definite
+		if(self.state < 0):
+			self.state = 0
+
 		return self.state
+
+class House(MarkovSource):
+	# houses have fairly high variance, but low average consumption
+	def __init__(self, meanFunction = lambda x : 5*math.sin(x*math.pi/12) + 20, variance = 4, initialCondition = 20):
+		MarkovSource.__init__(self, meanFunction, variance, initialCondition)
+
+class Factory(MarkovSource):
+	# factories have fairly low variance, but high average consumption
+	def __init__(self, meanFunction = lambda x : 100, variance = 0.5, initialCondition = 100):
+		MarkovSource.__init__(self, meanFunction, variance, initialCondition)
