1464f5585a7a48db234a96ba8423f694e96cd3bf
duncan <duncanmays@outlook.com>
2019-11-21 16:41:53 -0500

    added machanism to calculate price of production

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import math
 # this array holds all the times throughout the day that our model will iterate
 # right now I've set it to 15 minute intervals, we should take care in other 
 # parts of the program to allow intervals of other sizes
+# arange does the same thing as linspace in Matlab
 T = arange(0, 24, 0.25)
 
 # baseline power production, this will be the amount of electricity produced by nuclear
@@ -38,8 +39,23 @@ for i in range(0,numSolarPanels):
 for i in range(0,numWindTurbines):
 	stochasticProducers.append(WindTurbine())
 
+# the price of production at a given interval
+priceOfProduction = 0
+
+# the price of production for various methods
+# made these numbers up, we should run some kind of regression on IESO
+# data to get the actual numbers
+nuclearPrice = 1
+hydroPrice = 2
+gasPrice = 3
+# I am assuming that it costs the same amount of money to produce electricity
+# by both wind and solar, this is probably a bad assumption
+renewablePrice = 0.1
+
+# arrays that will be used to plot, serve no other purpose than this
 demand = []
 production = []
+prodPrice = []
 
 # main program loop
 # each iteration of this loop represents one day in the model
@@ -49,14 +65,20 @@ for MAX in range(0,1):
 	for t in T:
 		totalProduction = 0
 		totalDemand = 0
+		priceOfProduction = 0
 
-		# production from nuclear and hydro plants is added to the grid
+		# production from nuclear and hydro plants is added to the grid, 
+		# with necessary adjustments to priceOfProduction
 		totalProduction += baseline
+		priceOfProduction += nuclearPrice*baseline
 		totalProduction += hydroSchedule(t)
+		priceOfProduction += hydroPrice*hydroSchedule(t)
 
 		# production from solar panels and wind turbines is added to the grid
 		for producer in stochasticProducers:
-			totalProduction += producer.update(t)
+			temp = producer.update(t)
+			totalProduction += temp
+			priceOfProduction += renewablePrice*temp
 
 		# users draw electricity from grid
 		for consumer in stochasticConsumers:
@@ -64,12 +86,18 @@ for MAX in range(0,1):
 
 		# production from gas-fired plants is added to the grid
 		if(totalDemand > totalProduction):
-			totalProduction = totalDemand
+			temp = totalDemand - totalProduction
+			totalProduction += temp
+			priceOfProduction += gasPrice*temp
 
+
+		# these lines only serve to make plots below
 		demand.append(totalDemand)
 		production.append(totalProduction)
+		prodPrice.append(priceOfProduction)
 
-
+# plots everything all nice and pretty
 plt.plot(T, demand)
 plt.plot(T, production)
+plt.plot(T,prodPrice)
 plt.show()
