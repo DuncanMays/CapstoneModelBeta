925ae07f03a28975a012a3065693ced4aa4e549b
Duncan <duncanmays@outlook.com>
2020-02-27 14:15:31 -0500

    in max folder, the agent class is typed up, have yet to test it

---
+++ QlearningExperiments/Max/maxQLearning.py
@@ -0,0 +1,122 @@
+
+from random import uniform, choice
+
+class QLearningAgent:
+	
+	def __init__(self, actions=[0,1], beta=0.25, rho=0.1, delta = 0.1):
+		self.Q = {}
+		self.actions = actions
+		self.beta = beta
+		self.rho = rho
+
+		# the tolerance of the agent to deviations from percieved optimality
+		self.delta = delta
+		# used to indicate weather or not the agent is currently learning
+		self.exploring = False
+
+		self.secondLastState = 'bootstrap'
+		self.secondLastAction = 'bootstrap'
+		self.lastState = 'bootstrap'
+		self.lastAction = 'bootstrap'
+
+		self.getAction(['bootstrap'])
+
+	def newLeaf(self, state):
+		if (len(state) > 0):
+			return {state[0]: self.newLeaf(state[1:len(state)])}
+		else:
+			leaf = {}
+			for i in self.actions:
+				leaf[i] = {'reward':uniform(-1, 1), 'alpha':1}
+
+			# policy is randomly initialized
+			leaf['policy'] = choice(self.actions)
+			# this indicates that the dict is indeed a leaf, rather than just another node in the Qtree
+			leaf['type'] = 'leaf'
+
+			return leaf
+
+	def getLeaf(self, state, q):
+		if(len(state) > 0):
+			try:
+				return self.getLeaf(state[1:len(state)], q[state[0]])
+			except(KeyError):
+				q[state[0]] = self.newLeaf(state[1:len(state)])
+				return self.getLeaf(state[1:len(state)], q[state[0]])
+		else:
+			return q
+
+	def getAction(self, state):
+		if (uniform(0, 1) < self.rho):
+			# small probability of selecting a random acion
+			action = choice(self.actions)
+		else:
+			# simply follows policy
+			action = self.getLeaf(state, self.Q)['policy']
+
+		self.secondLastState = self.lastState
+		self.secondLastAction = self.lastAction
+		self.lastState = state
+		self.lastAction = action
+
+		return action
+
+	def giveReward(self, reward):
+		lastLeaf = self.getLeaf(self.lastState, self.Q)
+
+		alpha = lastLeaf[self.lastAction]['alpha']
+		beta = 1 - alpha
+		temp = beta*lastLeaf[self.lastAction]['reward'] + alpha*reward
+		lastLeaf[self.lastAction]['reward'] = temp
+
+		secondlastLeaf = self.getLeaf(self.secondLastState, self.Q)
+		alpha = secondlastLeaf[self.lastAction]['alpha']*self.beta
+		secondlastLeaf[self.secondLastAction]['reward'] += alpha*temp 
+
+		newDenominator = 1/lastLeaf[self.lastAction]['alpha'] + 1
+		lastLeaf[self.lastAction]['alpha'] = 1/newDenominator
+
+	# this method is called when the agent is finished exploring
+	def finishExploring(self):
+		self.exploring = False
+
+		# updates the policy key in all leaves in the Qtree, it also resets
+		#  all alpha values to 1 for every state/action combination.
+		updatePolicy(self.Q)
+
+	def updatePolicy(self, q):
+		if(('type' in q) && (q['type'] == 'leaf')):
+			# this block will execute if q is a leaf
+
+			# iterates accross the actions for two reasons:
+			#  the first is resets their alpha values to 1
+			#  the second is to find the action with the maximal expected reward
+			optimalAction = self.actions[0]
+			for i in q:
+				# resets alpha
+				i['alpha'] = 1
+				# checks if i has a greater expected reward than the optimal
+				#  action, if it does, set optimalAction to i
+				if (q[i]['reward'] > q[optimalAction]['reward']):
+					optimalAction = i
+
+			# this set will hold the actions that have expected reward within
+			#  self.delta of the optimal action
+			A = []
+			for i in q:
+				if (q[i]['reward'] > q[optimalAction]['reward']+self.delta):
+					A.append(i)
+
+			# sets the policy for the state this leaf corresponds to to a random
+			#  element from A
+			q['policy'] = choice(A)
+
+		else:
+			# this block will execute if q is a node in the Qtree
+			for i in q:
+				# recursively calls updatePolicy on all branches off of q
+				updatePolicy(q)
+
+	# this method is called when the agent begins exploring
+	def startExploring(self):
+		self.exploring = True
---
+++ QlearningExperiments/Max/testingEnvironment.py
@@ -0,0 +1,107 @@
+from random import normalvariate, randint
+from QLearningAgent import QLearningAgent
+from matplotlib import pyplot as plt
+from numpy import arange
+
+# the actions that the agent will be allowed to perform
+actions = ['forward', 'backward', 'stay still']
+
+# the state dimensions
+time = arange(-12, 12, 1)
+localDemand = arange(-2, 2, 1)
+priceOfProduction = arange(-5, 5, 1)
+priceOfRetail = arange(-5, 5, 1)
+charge = arange(-3, 3, 1)
+
+agent = QLearningAgent(actions)
+
+# this list will hold the reward the agent recieves at each timestep, it will be used to plot the agent's performance
+performance = []
+
+# the state variables are randomly initialized
+timeIndex = randint(0, len(time))-1
+localDemandIndex = randint(0, len(localDemand))-1
+priceOfProductionIndex = randint(0, len(priceOfProduction))-1
+priceOfRetailIndex = randint(0, len(priceOfRetail))-1
+chargeIndex = randint(0, len(charge))-1
+
+print('running simulation')
+
+numIterations = 300000
+for i in range(0,numIterations):
+
+	# resets reward
+	reward = 0
+
+	# gets action from agent
+	action = agent.getAction([time[timeIndex], localDemand[localDemandIndex], priceOfProduction[priceOfProductionIndex], priceOfRetail[priceOfRetailIndex], charge[chargeIndex]])
+
+	if (action == 'forward'):
+		# the index in each dimenison is incremented upwards
+		timeIndex = (timeIndex+1)%len(time)
+		localDemandIndex = (localDemandIndex+1)%len(localDemand)
+		priceOfProductionIndex = (priceOfProductionIndex+1)%len(priceOfProduction)
+		priceOfRetailIndex = (priceOfRetailIndex+1)%len(priceOfRetail)
+		chargeIndex = (chargeIndex+1)%len(charge)
+
+	elif (action == 'backward'):
+		# the agent to recieves an award
+		reward = time[timeIndex] + localDemand[localDemandIndex] + priceOfProduction[priceOfProductionIndex] + priceOfRetail[priceOfRetailIndex] + charge[chargeIndex]
+
+		# the index in each dimenison is incremented downwards, with no reward, this will test if the agent can think ahead
+		timeIndex = (timeIndex-1)%len(time)
+		localDemandIndex = (localDemandIndex-1)%len(localDemand)
+		priceOfProductionIndex = (priceOfProductionIndex-1)%len(priceOfProduction)
+		priceOfRetailIndex = (priceOfRetailIndex-1)%len(priceOfRetail)
+		chargeIndex = (chargeIndex-1)%len(charge)
+
+	elif (action == 'stay still'):
+		pass
+
+	else:
+		print("this shouldn't happen")
+
+	# the state variables are randomly altered
+	timeIndex = (timeIndex+randint(-1,1))%len(time)
+	localDemandIndex = (localDemandIndex+randint(-1,1))%len(localDemand)
+	priceOfProductionIndex = (priceOfProductionIndex+randint(-1,1))%len(priceOfProduction)
+	priceOfRetailIndex = (priceOfRetailIndex+randint(-1,1))%len(priceOfRetail)
+	chargeIndex = (chargeIndex+randint(-1,1))%len(charge)
+
+	# prints progress, WILL NOT WORK WITH PYTHON2
+	# erases the last output
+	print('\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b', end = '')
+	# produces new output
+	print(' %'+str(100*i/numIterations), end = '')
+
+	# gives agent the reward
+	agent.giveReward(reward)
+
+	# adds reward to parformance so that it may be plotted
+	performance.append(reward)
+
+# time to calculate the average reward across a time period of num iterations
+# adds new line
+print()
+print('forming results')
+num = 300
+toPlot = []
+numIterations = len(performance)-num
+for i in range(0, numIterations):
+	sum = 0
+	for j in range(i,i+num):
+		sum += performance[j]
+	toPlot.append(sum/num)
+
+	# prints progress, WILL NOT WORK WITH PYTHON2
+	# erases the last output
+	print('\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b', end = '')
+	# produces new output
+	print(' %'+str(100*i/numIterations), end = '')
+
+# adds new line
+print()
+
+x = range(0, len(toPlot))
+plt.plot(x, toPlot)
+plt.show()
